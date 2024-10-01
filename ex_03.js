b746f26fbe1ed687e717097aab24702f3db353a1
AldricGarcia <aldric.garcia@epitech.digital>
2024-10-01 11:07:07 +0200

    firstcommit

---
+++ ex_03.js
@@ -0,0 +1,21 @@
+function changebackground() 
+{
+  let magicNumber = 3;
+
+  if (magicNumber === 3)
+  {
+    paintYellowRectangle();
+  }
+  magicNumber = 2;
+  if (magicNumber === 2) 
+  {
+    paintGreenRectangle();
+  }
+  magicNumber = 1;
+  if (magicNumber === 1)
+  {
+    paintRedRectangle();
+  }
+}
+
+changebackground();
